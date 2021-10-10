5ca01080c9229fc240f1d2903aa8a1b314a6ce4f
kimchanghwan <hubaimaster@gmail.com>
2021-10-10 13:17:47 +0900

    Chapter2: RN Naviagation HOME

--- App.js
+++ App.js
@@ -6,23 +6,31 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  Text,
-  View,
-} from 'react-native';
+// In App.js in a new project
 
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default App = () => {
-  return (
-      <SafeAreaView>
-        <View>
-          <Text>
-            순수한 메인이다.
-          </Text>
+function HomeScreen() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Home Screen</Text>
         </View>
-      </SafeAreaView>
+    );
+}
 
-  );
-};
+const Stack = createNativeStackNavigator();
+
+function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomeScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default App;
