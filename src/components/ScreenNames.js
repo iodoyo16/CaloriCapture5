2181f91f07e7e9cc6c147ea1ec18bd8b1c067935
sang-young-Moon <58156471+iodoyo16@users.noreply.github.com>
2021-12-05 15:37:51 +0900

    sangyoung-registerpage
    registerpage done/ organize member pages

--- src/components/ScreenNames.js
+++ src/components/ScreenNames.js
@@ -5,7 +5,8 @@ export default class ScreenNames{
     static NextFoodScreenName = '홈';
     static CameraScreenName = '식단 촬영';
     static HistoryScreenName = '나의 기록';
-
+    static RegisterScreenName='회원 가입';
+    static LoginScreenName='로그인';
     static FoodDetailScreenName = '음식 인식 결과';
 
 }
