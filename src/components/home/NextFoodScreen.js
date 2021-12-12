c68744703815061f311fd7e33e8d3e0a48938fcc
sang-young-Moon <58156471+iodoyo16@users.noreply.github.com>
2021-12-12 21:45:05 +0900

    dd
    dd

--- src/components/home/NextFoodScreen.js
+++ src/components/home/NextFoodScreen.js
@@ -14,66 +14,6 @@ import User from "../../model/User";
 //Home SCREEN
 export default function NextFoodScreen({route, navigation}) {
     //DATA EXAMPLE
-    /*
-    const data = [ 30, 60, 90 ]
-    const meal= ["아침", "점심", "저녁"]
-    const data_kcal = [ 20, 50, 70 ]
-    const data_protien = [ 45, 60, 70 ]
-    const axesSvg = { fontSize: 14, fill: '#5048e5' };
-    const verticalContentInset = { top: 5, bottom: 5 }
-    const xAxisHeight = 20
-    // JUN WOKRING
-    return <View style={styles.Container}>
-        <View style = {styles.header}>
-            <Text style={styles.TodaysMeal}>Today's MEAL</Text>
-        </View>
-        <View style={styles.body1}>
-            <Image style={styles.item3}
-                   source={require('../Food_Photo/meal3.jpeg')}/>
-            <Image style={styles.item2}
-                   source={require('../Food_Photo/meal2.jpeg')}/>
-            <Image style={styles.item1}
-                   source={require('../Food_Photo/meal1.jpeg')}/>
-        </View>
-        <View style={styles.body2}>
-            <View style={styles.GraphBox}>
-                <Text style={styles.NutritionData}>Today's Nutrition Data</Text>
-                <View style={{ height: "100%", padding: 20, flexDirection: 'row', top: 20, }}>
-                    <YAxis
-                        data={data}
-                        style={{ marginBottom: xAxisHeight }}
-                        contentInset={verticalContentInset}
-                        svg={axesSvg}
-                    />
-                    <View style={{ flex: 1, marginLeft: 10 }}>
-                        <LineChart
-                            style={{ flex: 1}}
-                            data={data_protien}
-                            contentInset={verticalContentInset}
-                            svg={{ stroke: 'rgb(134, 65, 244)' }}
-                        >
-                            <Grid/>
-                        </LineChart>
-
-                        <XAxis
-                            style={{ marginHorizontal: -10, height: xAxisHeight }}
-                            data={meal}
-                            formatLabel={(value, index) => value}
-                            contentInset={{ left: 10, right: 10 }}
-                            svg={axesSvg}
-                        />
-                    </View>
-                </View>
-
-            </View>
-        </View>
-        <View style={styles.body3}>
-            <TouchableOpacity style = {styles.NextMealRecomendationBtn}>
-                <Text style = {styles.NextMealRecomendationTxt}>Next Meal Recomendation</Text>
-            </TouchableOpacity>
-        </View>
-    </View>
-*/
     const oneDayInfo=HistoryStorage.getHistory(User.getMyId())[`${HistoryInfo.convertDateFormat(new Date())}`];
     const [myDetailHistory, setMyDetailHistory] = useState([]);
     const [myMealsTotal, setMyMealsTotal] = useState({});
@@ -113,7 +53,7 @@ export default function NextFoodScreen({route, navigation}) {
             <View style={styles.body2} showsVerticalScrollIndicator={true}>
                 <ScrollView style={{flexDirection: "column"}}>
                     <DetailNutritionGraph
-                        dateString={dateString}
+                        dateString='Today'
                         myDetailHistory={myDetailHistory}
                         myMealsTotal={myMealsTotal}
                         oneDayInfo={oneDayInfo}/>
@@ -127,6 +67,7 @@ export default function NextFoodScreen({route, navigation}) {
         </SafeAreaView>
     )
 }
+
 const styles = StyleSheet.create({ //Screen View Components - JUN
 
     Container: { //container
@@ -142,43 +83,30 @@ const styles = StyleSheet.create({ //Screen View Components - JUN
     body1: { //PREVIOUS MEAL PHOTO
         flex: 2,
         flexDirection: 'row',
-        justifyContent: 'space-around',
-
+        //justifyContent: 'space-around',
+        paddingBottom:10,
         backgroundColor: '#ffffff',
-
+        //backgroundColor:'red'
     },
     body2: { //GRAPH
-        flex: 5,
+        flex:5,
         backgroundColor: '#ffffff',
-        justifyContent: 'center',
-        alignItems: 'center',
-
+        paddingLeft:15,
         //borderRadius: '5%',
     },
     body3: { //NEXT MEAL RECOMENDATION
-        flex: 2,
+        flex: 1,
         backgroundColor: '#ffffff',
         alignItems: 'center',
         justifyContent: 'center',
     },
-    /*Screen ELEMENTS*/
     TodaysMeal: {
         width: '100%',
-        height: '80%',
+        height: '60%',
         textAlign: 'left',
         textAlignVertical: 'center',
         fontSize: 30,
         fontWeight: '700',
-        color: '#5048e5',
-    },
-    NutritionData: {
-        position: 'absolute',
-        width: '100%',
-        height: '80%',
-        textAlign: 'left',
-        textAlignVertical: 'center',
-        fontSize: 20,
-        fontWeight: '500',
         color: '#5048e5',
     },
     NextMealRecomendationBtn: {
@@ -186,7 +114,7 @@ const styles = StyleSheet.create({ //Screen View Components - JUN
         borderRadius: 10,
         width: '80%',
         height: '60%',
-        marginBottom:20,
+        marginBottom: 20,
         alignItems: "center",
         //shadow
         shadowOffset: {
@@ -208,10 +136,11 @@ const styles = StyleSheet.create({ //Screen View Components - JUN
         color: '#ffffff',
     },
     item1: {
-        //flex: 1,
+        marginBottom:5,
+        marginRight:5,
         backgroundColor: '#cccccc',
-        height: '90%',
-        width: '30%',
+        height: '100%',
+        width: 100,
         //shadow
         shadowOffset: {
             width: 0,
@@ -221,47 +150,4 @@ const styles = StyleSheet.create({ //Screen View Components - JUN
         shadowRadius: 9.0,
         elevation: 15,
     },
-    item2: {
-        //flex: 1,
-        height: '90%',
-        width: '30%',
-        backgroundColor: '#cccccc',
-        //shadow
-        shadowOffset: {
-            width: 0,
-            height: 7,
-        },
-        shadowOpacity: 0.35,
-        shadowRadius: 9.0,
-        elevation: 15,
-    },
-    item3: {
-        //flex: 1,
-        height: '90%',
-        width: '30%',
-        backgroundColor: '#cccccc',
-        //shadow
-        shadowOffset: {
-            width: 0,
-            height: 7,
-        },
-        shadowOpacity: 0.35,
-        shadowRadius: 9.0,
-        elevation: 15,
-    },
-    GraphBox: {
-      height: '90%',
-        width: '90%',
-        backgroundColor: '#ececec',
-        borderRadius: 5,
-        //shadow
-        shadowOffset: {
-            width: 0,
-            height: 7,
-        },
-        shadowOpacity: 0.35,
-        shadowRadius: 9.0,
-        elevation: 15,
-    },
-
 });
